9b9dad7e699c3a36a18f43014577da25979374dc
raimeiga <yusuke612@hotmail.com>
2023-07-04 17:44:31 +0900

    引数・戻り値コード追加

--- js/function.js
+++ js/function.js
@@ -8,7 +8,7 @@ const sayGoodMorning = () => {
 // 夜のあいさつを出力する関数を定義する
 const sayGoodEvening = () => {
     console.log('こんばんは！');
-    console.log('今日も一日お疲れさまでした！');
+    console.log('今日も一日お疲れさまでした。！');
 }
 
 // 朝のあいさつを出力する関数を呼び出す
@@ -16,3 +16,27 @@ sayGoodMorning();
 
 // 夜のあいさつを出力する関数を呼び出す
 sayGoodEvening();
+
+
+// 与えられた引数priceに送料を加算し、その値を出力する関数を定義する
+const caluculateTotal = (price) => {
+    console.log(price + 500 + '円');
+}
+
+// 関数を呼び出し、引数として購入金額を渡す
+caluculateTotal(1200);
+
+const addTwoArguments = (price, shippingFee) => {
+    console.log(price + shippingFee + '円');
+}
+
+// 関数を呼び出し、引数として購入金額を渡す
+addTwoArguments(1200, 500);
+
+// 与えられた引数numを2倍にし、その値を戻り値として返す関数を定義する
+const double = (num) => {
+    return num * 2;
+  }
+  
+  // 関数の戻り値を出力する
+  console.log(double(30));
