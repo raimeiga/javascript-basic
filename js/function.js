19d8991f6bb28127c52530e9d626064557a0c2d8
raimeiga <yusuke612@hotmail.com>
2023-07-04 18:15:53 +0900

    スコープを理解するためのコード追加

--- js/function.js
+++ js/function.js
@@ -40,3 +40,17 @@ const double = (num) => {
   
   // 関数の戻り値を出力する
   console.log(double(30));
+
+
+// 関数の中で定数を宣言し、関数の中で定数を使う
+const useVariable = () => {
+    const userName = '侍太郎'
+    console.log(userName);
+  }
+  
+  // 関数を呼び出す
+  useVariable();
+
+  // 関数の中で宣言した定数を関数の外で使う
+ console.log(userName);
+  
