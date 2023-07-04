f4e9cb23545ac63499df5b85870df4c82bded4fb
raimeiga <yusuke612@hotmail.com>
2023-07-04 15:21:26 +0900

    objectを書きだした

---
+++ js/object.js
@@ -0,0 +1,16 @@
+// オブジェクトの宣言と値の代入を行う
+const personalDate = { name:'侍太郎', age:36, gender:'男性' };
+
+// オブジェクトの値を出力する
+console.log(personalDate);
+
+// 'age'というキーの値を更新する
+personalDate.age = 37;
+
+// 新しくプロパティ（キーと値）を追加する
+personalDate.adress = '東京都';
+
+// オブジェクトの値を出力する
+console.log(personalDate);
+
+console.log(personalDate.gender);
